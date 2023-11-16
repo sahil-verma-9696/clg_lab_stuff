45b71d288840c19d6165b80aa4031099d953b24d
sahil-verma-9696 <laptopsahil123@gmail.com>
2023-11-16 19:26:11 +0530

    college work

--- python_lab/16-11/index.py
+++ python_lab/16-11/index.py
@@ -1,8 +1,41 @@
-def pattern(n):
+def starPattern(n):
     space=n-2
-    num=1
+    star=1
     for x in range(1,n):
-        print("a"*space+str(num)*num)
+        print(" "*space+"*"*star)
         space=space-1
-        num=num+1
-pattern(7)
+        star=star+2
+
+def numPattern(n):
+    space = n-1
+    num = 1
+    timesOfNum = 1
+    for x in range(n):
+        strNum = str(num)
+        print(" "*space + strNum*timesOfNum )
+        num = num+1
+        timesOfNum = timesOfNum + 2
+        space = space-1
+
+def reverseStarPattern(n):
+    space = 0 
+    star = n + (n-1)
+    for x in range(n):
+        print(" "*space + "*"*star)
+        space = space + 1
+        star = star-2    
+
+def reverseNumPattern(n):
+    space = 0
+    num = n
+    timesOfNum = n + (n-1)   
+    for x in range(n):
+        print(" "*space + str(num)*timesOfNum)
+        num = num-1
+        space = space + 1
+        timesOfNum = timesOfNum - 2
+
+#reverseNumPattern(5)
+#reverseStarPattern(5)
+#numPattern(5)
+#starPattern(7)
